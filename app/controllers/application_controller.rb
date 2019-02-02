a5704f02c300790a036fe95f69090633b2e65235
Cheng Vue <csalvue@gmail.com>
2019-02-02 13:42:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
+  def cart
     session[:cart] ||= []
   end
 
-  def create
+  def add_to_cart
     cart << params[:product]
   end
 end
