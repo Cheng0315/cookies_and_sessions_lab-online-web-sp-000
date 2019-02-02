06ba82047424d9ff14e90da8d6e1880f55469cef
Cheng Vue <csalvue@gmail.com>
2019-02-02 13:54:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'application#cart'
-  post '/add_to_cart' => 'product#cart'
+  post '/add_to_cart' => 'products#cart'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
