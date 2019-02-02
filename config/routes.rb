e2a4e478301697c6fe55c3bcae44625bf3e7d77b
Cheng Vue <csalvue@gmail.com>
2019-02-02 13:40:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :application, only:[:cart, :create]
+  resources :application, only:[:cart, :add_to_cart]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
